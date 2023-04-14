8a39293414afc8982d9d322f96a46ebbf93d54b2
Juliano Sirtori <julianosirtori@gmail.com>
2023-04-14 11:22:33 -0300

    feat!: add timer

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const sum = (a,b) => {
 
 const main = () => {
   printLog('Hello World');
-  printLog(sum(1,2));
+  printLog(`1 + 2 is  ${sum(1,2)}`);
+  let sec = 1;
+  const intervalTime =  setInterval(() => {
+    printLog(`second: ${sec}`);
+    sec++;
+    if(sec === 5) clearInterval(intervalTime);
+  }, 1000)
 }
 
 main()
